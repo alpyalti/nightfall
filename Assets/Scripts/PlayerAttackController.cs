dcae564d5f1bad7efbf794badf178c1a3988e640
Alp Yaltı <45272167+alpyalti@users.noreply.github.com>
2024-04-05 19:34:03 +0300

    CameraFix

--- Assets/Scripts/PlayerAttackController.cs
+++ Assets/Scripts/PlayerAttackController.cs
@@ -1,8 +1,77 @@
+using System;
 using UnityEngine;
 
 public class PlayerAttackController : MonoBehaviour
 {
     public AxeScript axeScript; // Assign this in the inspector
+    private Animator anim;
+    public float cooldownTime = 2f;
+    private float nextFireTime = 0f;
+    public static int noOfClicks = 0;
+    float lastClickedTime = 0;
+    float maxComboDelay = 1;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+ 
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("hit"))
+        {
+            anim.SetBool("hit", false);
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
+        {
+            anim.SetBool("hit2", false);
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("hit3"))
+        {
+            anim.SetBool("hit3", false);
+            noOfClicks = 0;
+        }
+ 
+ 
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+ 
+        //cooldown time
+        if (Time.time > nextFireTime)
+        {
+            // Check for mouse input
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClick();
+ 
+            }
+        }
+    }
+    
+    void OnClick()
+    {
+        //so it looks at how many clicks have been made and if one animation has finished playing starts another one.
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if (noOfClicks == 1)
+        {
+            anim.SetBool("hit", true);
+        }
+        noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+ 
+        if (noOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("hit"))
+        {
+            anim.SetBool("hit", false);
+            anim.SetBool("hit2", true);
+        }
+        if (noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
+        {
+            anim.SetBool("hit2", false);
+            anim.SetBool("hit3", true);
+        }
+    }
 
     public void EnableAxeCollider()
     {
